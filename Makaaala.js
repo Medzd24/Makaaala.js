da431525f0c26e4f720927c36a7f34f875f725d0
Medzd24 <150199255+Medzd24@users.noreply.github.com>
2023-11-14 14:25:50 +0100

    Update Makaaala.js

--- Makaaala.js
+++ Makaaala.js
@@ -1,18 +1,32 @@
+ 
+        const bloggerId = '8781535279728903487';
+        const apiKey = 'AIzaSyCciRkKAcwlJJQ9v89lrJHQKnr7R8PpYeQ';
+        const apiUrl = `https://www.googleapis.com/blogger/v3/blogs/${bloggerId}/posts?key=${apiKey}&maxResults=4`;
 
-    const numberOfItems = 4;
-    const rssFeedUrl = "https://www.tun-24.com/rss.xml";
+        const postLinks = []; // مصفوفة لتخزين روابط المقالات
 
-    // جلب البيانات من مصدر RSS
-    fetch(`https://api.rss2json.com/v1/api.json?rss_url=${rssFeedUrl}`)
-        .then(response => response.json())
-        .then(data => {
-            const items = data.items.slice(1, numberOfItems);
+        async function fetchHeadlines() {
+            try {
+                const response = await fetch(apiUrl);
 
-            // تحديث الروابط والنصوص
-            items.forEach((item, index) => {
-                const headlineElement = document.getElementById(`headline${index + 0}`);
-                headlineElement.textContent = item.title;
-                headlineElement.href = item.link;
-            });
-        })
-        .catch(error => console.error("Error fetching RSS feed:", error));
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                const data = await response.json();
+
+                // تحديث العناوين والروابط
+                for (let i = 0; i < 4; i++) {
+                    const headlineElement = document.getElementById(`headline${i + 1}`);
+                    
+                    headlineElement.innerText = data.items[i].title;
+                    headlineElement.href = data.items[i].url;
+                    
+                    postLinks[i] = data.items[i].url; // تخزين الرابط في المصفوفة
+                }
+            } catch (error) {
+                console.error('حدث خطأ أثناء جلب العناوين:', error);
+            }
+        }
+
+        fetchHeadlines();
